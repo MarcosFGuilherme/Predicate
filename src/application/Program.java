d47a0d109be0dde8cd12de8d3f7bb9260d318a18
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-18 15:54:56 -0300

    Update - Implements RemoveIf().

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		showProduct(list);
+		//Remover todos os produtos com preco superior a 100.00
+		list.removeIf(p->p.getPrice() >= 100.0);
+		showProduct(list);
 	}
-
+	public static void showProduct(List<Product> list) {
+		list.sort((p1,p2)-> p1.getPrice().compareTo(p2.getPrice()));
+		for (Product p : list) {
+			System.out.println(p);
+		}
+		System.out.println();
+	}
 }
